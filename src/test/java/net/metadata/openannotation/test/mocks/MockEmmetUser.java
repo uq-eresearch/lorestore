c408ec3582e592e41fd8984dec97a28d22be2e8e
Anna Gerber <anna.m.gerber@gmail.com>
2012-02-28 10:05:49 +1000

    Remove hardcoded copyright comment (out of date)

--- src/test/java/net/metadata/openannotation/test/mocks/MockEmmetUser.java
+++ src/test/java/net/metadata/openannotation/test/mocks/MockEmmetUser.java
@@ -1,23 +1,3 @@
-/*
- * Copyright (c) 2008-2010 School of Information Technology and Electrical Engineering, 
- * The University of Queensland.  This software is being developed for the "Data 
- * Integration and Annotation Services in Biodiversity" (DIAS-B) project.  DIAS-B
- * is a NeAT project, funded jointly by ARCS and ANDS, and managed by the Atlas 
- * of Living Australia (ALA).
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 package net.metadata.openannotation.test.mocks;
 
 import java.sql.Timestamp;
