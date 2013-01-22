eafed27322eb3da005ed31543eb2a6cc73cc16cb
Anna Gerber <anna.m.gerber@gmail.com>
2013-01-23 09:47:55 +1000

    Use JSON-LD for JSON serialisation Use JSON LD context as recommended in new OA spec

---
+++ src/main/java/net/metadata/openannotation/lorestore/util/OAJSONLDSerializer.java
@@ -0,0 +1,114 @@
+package net.metadata.openannotation.lorestore.util;
+
+import org.ontoware.aifbcommons.collection.ClosableIterator;
+import org.ontoware.rdf2go.model.Model;
+import org.ontoware.rdf2go.model.ModelSet;
+import org.ontoware.rdf2go.model.Statement;
+import org.ontoware.rdf2go.model.node.DatatypeLiteral;
+import org.ontoware.rdf2go.model.node.LanguageTagLiteral;
+import org.ontoware.rdf2go.model.node.Literal;
+import org.ontoware.rdf2go.model.node.Resource;
+import org.ontoware.rdf2go.model.node.URI;
+import org.ontoware.rdf2go.model.node.Variable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import de.dfki.km.json.jsonld.JSONLDProcessingError;
+/* 
+ * Based on 
+ * https://github.com/ismriv/jsonld-rdf2go/blob/master/src/main/java/ie/deri/smile/jsonld/rdf2go/RDF2GoJSONLDSerializer.java
+ * 
+ * Modified fore lorestore to work with new JSONLD API and to ignore schema triples
+ * 
+ * Copyright (c) 2012 Ismael Rivera
+
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+public class OAJSONLDSerializer extends de.dfki.km.json.jsonld.JSONLDSerializer {
+
+        public void importModel(Model model) {
+            ClosableIterator<Statement> statements = model.iterator();
+            while (statements.hasNext()) {
+                handleStatement(statements.next());
+            }
+            statements.close();
+        }
+
+        public void importModelSet(ModelSet modelSet) {
+            ClosableIterator<Statement> statements = null;
+            
+            statements = modelSet.findStatements(Variable.ANY, Variable.ANY, Variable.ANY, Variable.ANY);
+            while (statements.hasNext()) {
+                handleStatement(statements.next());
+            }
+            statements.close();
+        }
+
+        public void handleStatement(Statement nextStatement) {
+            Resource subject = nextStatement.getSubject();
+            URI predicate = nextStatement.getPredicate();
+            Object object = nextStatement.getObject();
+            String subjString = subject.toString(),
+               predString = predicate.toString(),
+               objString = object.toString();
+            // don't serialize schema triples
+            if (subjString.startsWith("http://www.w3.org/2000/01/rdf-schema") ||
+                subjString.startsWith("http://www.w3.org/1999/02/22-rdf-syntax-ns") ||
+                subjString.startsWith("http://www.openannotation.org/ns/") ||
+                subjString.startsWith("http://purl.org/dc/terms/") ||
+                predString.startsWith("http://www.w3.org/2000/01/rdf-schema#subPropertyOf") ||
+                objString.startsWith("http://www.w3.org/2000/01/rdf-schema#Property") ||
+                objString.startsWith("http://www.w3.org/2000/01/rdf-schema#Resource") ||
+                objString.startsWith("http://www.w3.org/1999/02/22-rdf-syntax-ns")){
+                return;
+                
+            }
+            
+            if (object instanceof DatatypeLiteral) {
+                DatatypeLiteral literal = (DatatypeLiteral) object;
+                String value = literal.getValue();
+                String datatype = literal.getDatatype().toString();
+                triple(subjString, predString, value, datatype, null);
+            } else if (object instanceof LanguageTagLiteral) {
+                LanguageTagLiteral literal = (LanguageTagLiteral) object;
+                String value = literal.getValue();
+                triple(subjString, predString, value, null, literal.getLanguageTag());
+            } else if (object instanceof Literal) {
+                Literal literal = (Literal) object;
+                String value = literal.getValue();
+                triple(subjString, predString, value, null, null);
+            } else {
+                triple(subjString, predString, objString);
+            }
+        }
+
+        @Override
+        public void parse(Object input) throws JSONLDProcessingError {
+            if (input == null){
+                return;
+            }
+            if (input instanceof Model) {
+                importModel((Model)input);
+            } else if (input instanceof ModelSet){
+                importModelSet((ModelSet) input);
+            } else {
+                throw new JSONLDProcessingError("JSON LD Serializer expects Model or ModelSet input");
+            }
+        }
+}
--- src/main/java/net/metadata/openannotation/lorestore/views/OACNamedGraphsView.java
+++ src/main/java/net/metadata/openannotation/lorestore/views/OACNamedGraphsView.java
@@ -5,177 +5,167 @@ import static net.metadata.openannotation.lorestore.common.LoreStoreConstants.LO
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
-import java.io.StringReader;
 import java.io.StringWriter;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.xml.transform.Source;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import javax.xml.transform.stream.StreamSource;
+
+import net.metadata.openannotation.lorestore.util.OAJSONLDSerializer;
 
 import org.apache.log4j.Logger;
-import org.apache.xerces.parsers.DOMParser;
 import org.ontoware.rdf2go.model.ModelSet;
 import org.ontoware.rdf2go.model.Syntax;
-import org.xml.sax.InputSource;
 
 import au.edu.diasb.chico.mvc.BaseView;
 import au.edu.diasb.chico.mvc.MimeTypes;
+import de.dfki.km.json.JSONUtils;
+import de.dfki.km.json.jsonld.JSONLD;
+import de.dfki.km.json.jsonld.JSONLDProcessor.Options;
 
 public class OACNamedGraphsView extends BaseView {
-	 	public OACNamedGraphsView() {
-	        super(Logger.getLogger(OACNamedGraphsView.class));
-	    }
-	 	
-	 	@Override
-	    protected void renderMergedOutputModel(Map<String, Object> map, 
-	            HttpServletRequest request, HttpServletResponse response) 
-	    throws IOException {
-	 		logger.info("Annotations render");
-	 		ModelSet annotations = (ModelSet) map.get("annotations");
-	 		
-	 		
-	    	if (annotations == null || annotations.size() == 0){
-	    		logger.debug("Problem getting all annotations");
-	    	}
-	    	logger.debug("render merged output annotations: ");
-	        String stylesheetParam = request.getParameter(LORESTORE_USE_STYLESHEET);
-	        
-	        String stylesheetURI = null;
-	        
-	        OutputStream os = null;
-	        // FIXME the mapping of 'accept' types to formats that we understand and hence
-	        // to the content types that we use needs to be soft, and a lot smarter.
-	        // Think about using ContentNegotiatingViewResolver
-	        try {
-	            if (annotations != null) {
-            		// TriX or Trig are preferred format as they have named graph support,
-	            	// however RDF/XML is the default
-	            	if (isAcceptable(MimeTypes.XML_RDF_MIMETYPES, request)) {	
-	                	stylesheetURI = (stylesheetParam == null || stylesheetParam.length() == 0) ?
-	                			"/lorestore/stylesheets/OAC.xsl" : stylesheetParam;
-	                	
-	                	if (isAcceptable(MimeTypes.XML_MIMETYPE, request)){
-	                		// we don't provide HTML as yet, use XML with stylesheet instead
-	                		response.setContentType(MimeTypes.XML_MIMETYPE);
-	                	} else {
-	                		response.setContentType(MimeTypes.XML_RDF);
-	                	}
-	        	        response.setCharacterEncoding("UTF-8");
-	        	        os = outputRDF(response, annotations, stylesheetURI, Syntax.RdfXml);
-	                } else if (isAcceptable("application/trix",request)) {
-	                	// TODO: add default stylesheet for TriX
-	         	        response.setContentType(MimeTypes.XML_MIMETYPE);
-	        	        response.setCharacterEncoding("UTF-8");
-	        	        os = outputRDF(response, annotations, stylesheetURI, Syntax.Trix);
-	                } else if (isAcceptable("application/x-trig", request)){
-	                	
-	                	response.setContentType("application/x-trig");
-	                	os = outputRDF(response, annotations, null, Syntax.Trig);
-	                } else if (isAcceptable("application/json",request)){
-	                	response.setContentType("application/json");
-	                	os = outputJSON(response, annotations);
-	                } else {
-	                    response.sendError(HttpServletResponse.SC_NOT_ACCEPTABLE,
-	                            "Request response only available in Trix, RDF+XML, Trig and JSON formats");
-	                }
-	            } else {
-	                logger.error("No content has been set");
-	                response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, 
-	                        "No content has been set");
-	            }
-	        } finally {
-	            if (os != null) {
-	            	os.flush();
-	                os.close();
-	            }
-	            if (annotations != null){
-	            	try {
-	            		annotations.commit();
-	            		annotations.close();
-	            	} catch (Exception e){
-	            		logger.error("OACNamedGraphsView " + e.getMessage());
-	            	};
-	            }
-	        }
-	 }
 
-	 private OutputStream outputRDF(HttpServletResponse response,  
-	    		ModelSet annotations, String stylesheetURL, Syntax syntax) 
-	    throws IOException {
+         public OACNamedGraphsView() {
+                super(Logger.getLogger(OACNamedGraphsView.class));
+         }
+         
+         @Override
+        protected void renderMergedOutputModel(Map<String, Object> map, 
+                HttpServletRequest request, HttpServletResponse response) 
+        throws IOException {
+             logger.info("Annotations render");
+             ModelSet annotations = (ModelSet) map.get("annotations");
+             
+             
+            if (annotations == null || annotations.size() == 0){
+                logger.debug("Problem getting all annotations");
+            }
+            logger.debug("render merged output annotations: ");
+            String stylesheetParam = request.getParameter(LORESTORE_USE_STYLESHEET);
+            
+            String stylesheetURI = null;
+            
+            OutputStream os = null;
+            // FIXME the mapping of 'accept' types to formats that we understand and hence
+            // to the content types that we use needs to be soft, and a lot smarter.
+            // Think about using ContentNegotiatingViewResolver
+            try {
+                if (annotations != null) {
+                    // TriX or Trig are preferred format as they have named graph support,
+                    // however RDF/XML is the default
+                    if (isAcceptable(MimeTypes.XML_RDF_MIMETYPES, request)) {    
+                        stylesheetURI = (stylesheetParam == null || stylesheetParam.length() == 0) ?
+                                "/lorestore/stylesheets/OAC.xsl" : stylesheetParam;
+                        
+                        if (isAcceptable(MimeTypes.XML_MIMETYPE, request)){
+                            // we don't provide HTML as yet, use XML with stylesheet instead
+                            response.setContentType(MimeTypes.XML_MIMETYPE);
+                        } else {
+                            response.setContentType(MimeTypes.XML_RDF);
+                        }
+                        response.setCharacterEncoding("UTF-8");
+                        os = outputRDF(response, annotations, stylesheetURI, Syntax.RdfXml);
+                    } else if (isAcceptable("application/trix",request)) {
+                        // TODO: add default stylesheet for TriX
+                         response.setContentType(MimeTypes.XML_MIMETYPE);
+                        response.setCharacterEncoding("UTF-8");
+                        os = outputRDF(response, annotations, stylesheetURI, Syntax.Trix);
+                    } else if (isAcceptable("application/x-trig", request)){
+                        
+                        response.setContentType("application/x-trig");
+                        os = outputRDF(response, annotations, null, Syntax.Trig);
+                    } else if (isAcceptable("application/json",request)){
+                        response.setContentType("application/json");
+                        os = outputJSON(response, annotations);
+                    } else {
+                        response.sendError(HttpServletResponse.SC_NOT_ACCEPTABLE,
+                                "Request response only available in Trix, RDF+XML, Trig and JSON formats");
+                    }
+                } else {
+                    logger.error("No content has been set");
+                    response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, 
+                            "No content has been set");
+                }
+            } finally {
+                if (os != null) {
+                    os.flush();
+                    os.close();
+                }
+                if (annotations != null){
+                    try {
+                        annotations.commit();
+                        annotations.close();
+                    } catch (Exception e){
+                        logger.error("OACNamedGraphsView " + e.getMessage());
+                    };
+                }
+            }
+     }
 
-	        if (stylesheetURL == null) {
-	            OutputStream os = response.getOutputStream();
-	            annotations.writeTo(os,syntax);
-	            return os;
-	        } else {
+     private OutputStream outputRDF(HttpServletResponse response,  
+                ModelSet annotations, String stylesheetURL, Syntax syntax) 
+        throws IOException {
 
-	            // I considered tweaking the RDF serializers to add the stylesheet processing
-	            // instruction.  Sesame would support this, but it would be difficult with Jena.
-	            // Instead, we serialize to a StringBuffer and do some simple surgery to insert
-	            // the required stuff.
-	            StringWriter sw = new StringWriter();
-	            annotations.writeTo(sw, syntax);
-	            StringBuffer sb = sw.getBuffer();
-	            // The insertion point will be immediately after the '?>' of the xml declaration
-	            // (if present) or at the start of the document.
-	            int insertionPoint = 0;
-	            int xmlDeclStart = sb.indexOf("<?xml ");
-	            if (xmlDeclStart != -1) {
-	                insertionPoint = sb.indexOf("?>", xmlDeclStart);
-	                if (insertionPoint == -1) {
-	                    logger.error("Malformed XML decl");
-	                    response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, 
-	                            "Malformed XML decl");
-	                    return null;
-	                } else {
-	                    insertionPoint += "?>".length();
-	                }
-	            }
-	            // The inserted text may include an xml declaration.  (Some RDF/XML serializers
-	            // don't include one by default.)
-	            String insertion = ((insertionPoint == 0) ? "<?xml version=\"1.0\" encoding=\"UTF-8\"?>": "") +
-	                    "\n<?xml-stylesheet type=\"text/xsl\" href=\"" + stylesheetURL + "\"?>\n";
-	            sb.insert(insertionPoint, insertion);
-	            OutputStream os = response.getOutputStream();
-	            os.write(sb.toString().getBytes());
-	            return os;
-	        }
-	    }
-	 private OutputStream outputJSON(HttpServletResponse response, ModelSet annotations) 
-	    throws IOException {
-		 
-		 OutputStream os = response.getOutputStream();
-		 StringBuffer json = new StringBuffer();
-	     try{
-			 DOMParser dp = new DOMParser();
-			 ClassLoader cl = this.getClass().getClassLoader();
-		     java.io.InputStream in = cl.getResourceAsStream("OAC-to-JSON.xsl");
-		     Source xslt = new StreamSource(in);
-		     Transformer trans = TransformerFactory.newInstance().newTransformer(xslt);
-		     String rdfXML = annotations.serialize(Syntax.RdfXml);
+            if (stylesheetURL == null) {
+                OutputStream os = response.getOutputStream();
+                annotations.writeTo(os,syntax);
+                return os;
+            } else {
 
-		     // process Model to produce custom json format
-		     StringWriter writer = new StringWriter();
-			     
-			 dp.parse(new InputSource(new StringReader(rdfXML)));
-   
-			 trans.transform(new DOMSource(dp.getDocument()),  new StreamResult(writer));
-		     json.append(writer.toString());
+                // I considered tweaking the RDF serializers to add the stylesheet processing
+                // instruction.  Sesame would support this, but it would be difficult with Jena.
+                // Instead, we serialize to a StringBuffer and do some simple surgery to insert
+                // the required stuff.
+                StringWriter sw = new StringWriter();
+                annotations.writeTo(sw, syntax);
+                StringBuffer sb = sw.getBuffer();
+                // The insertion point will be immediately after the '?>' of the xml declaration
+                // (if present) or at the start of the document.
+                int insertionPoint = 0;
+                int xmlDeclStart = sb.indexOf("<?xml ");
+                if (xmlDeclStart != -1) {
+                    insertionPoint = sb.indexOf("?>", xmlDeclStart);
+                    if (insertionPoint == -1) {
+                        logger.error("Malformed XML decl");
+                        response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, 
+                                "Malformed XML decl");
+                        return null;
+                    } else {
+                        insertionPoint += "?>".length();
+                    }
+                }
+                // The inserted text may include an xml declaration.  (Some RDF/XML serializers
+                // don't include one by default.)
+                String insertion = ((insertionPoint == 0) ? "<?xml version=\"1.0\" encoding=\"UTF-8\"?>": "") +
+                        "\n<?xml-stylesheet type=\"text/xsl\" href=\"" + stylesheetURL + "\"?>\n";
+                sb.insert(insertionPoint, insertion);
+                OutputStream os = response.getOutputStream();
+                os.write(sb.toString().getBytes());
+                return os;
+            }
+        }
+     private OutputStream outputJSON(HttpServletResponse response, ModelSet annotations) 
+        throws IOException {
+         
+         OutputStream os = response.getOutputStream();
+         StringBuffer jsonResult = new StringBuffer();
+         try{
+             OAJSONLDSerializer serializer = new OAJSONLDSerializer();
+             Object json = JSONLD.fromRDF(annotations, serializer);
+             Options opt = new Options();
+             opt.optimize = true;
+             ClassLoader cl = this.getClass().getClassLoader();
+             java.io.InputStream in = cl.getResourceAsStream("oacontext.json");
+             Object jsonContext = JSONUtils.fromInputStream(in);
+             jsonResult.append(JSONUtils.toString(JSONLD.compact(json, jsonContext, opt)));
+        } catch (Exception e){
+             logger.error("Error " + e.getMessage());
+        }
 
-	    } catch (Exception e){
-	    	 logger.debug(e.getMessage());
-	    }
-
-     	PrintStream printStream = new PrintStream(os);
-     	printStream.print(json);
-     	printStream.close();
-     	return os;
-	 }
-	
+         PrintStream printStream = new PrintStream(os);
+         printStream.print(jsonResult);
+         printStream.close();
+         return os;
+     }
+    
 }
